d697eb5f8e29a4b8333c7d1c5d8b5edd2b75a271
sushma0409 <108390485+sushma0409@users.noreply.github.com>
2022-06-30 20:17:32 +0530

    Update index.js

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,13 +1,26 @@
-/// <reference types="cypress" />
 // ***********************************************************
-// This example plugins/index.js can be used to load plugins
+// This example support/index.js is processed and
+// loaded automatically before your test files.
 //
-// You can change the location of this file or turn off loading
-// the plugins file with the 'pluginsFile' configuration option.
+// This is a great place to put global configuration and
+// behavior that modifies Cypress.
+//
+// You can change the location of this file or turn off
+// automatically serving support files with the
+// 'supportFile' configuration option.
 //
 // You can read more here:
-// https://on.cypress.io/plugins-guide
+// https://on.cypress.io/configuration
 // ***********************************************************
+
+// Import commands.js using ES2015 syntax:
+import './commands'
+
+// Alternatively you can use CommonJS syntax:
+// require('./commands')
+
+
+import './functions'
 
 // This function is called when a project is opened or re-opened (e.g. due to
 // the project's config changing)
@@ -15,7 +28,6 @@
 /**
  * @type {Cypress.PluginConfig}
  */
-// eslint-disable-next-line no-unused-vars
 module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
